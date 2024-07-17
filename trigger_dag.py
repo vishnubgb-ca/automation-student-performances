6aeb967b7f8d34794a7a7dc4be07ca1650293593
www-data <www-data@ip-172-31-45-32.ec2.internal>
2024-07-17 14:01:43 +0000

    Added the required files

--- trigger_dag.py
+++ trigger_dag.py
@@ -2,7 +2,7 @@ import paramiko
 import os
 import requests
 deployment_url = os.environ["airflow_url"]
-dag_id = "automation-student-performances"
+dag_id = "automation-student-performances_Testtest"
 response = requests.post(
     url=f"{deployment_url}/api/v1/dags/{dag_id}/dagRuns",
     auth=(os.environ['airflow_username'], os.environ['airflow_password']),
